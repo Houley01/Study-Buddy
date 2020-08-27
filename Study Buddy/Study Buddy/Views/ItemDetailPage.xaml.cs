90abe4db26442026faa9959ba2e3805b46a6b298
Houley <ethouley@gmail.com>
2020-08-27 21:14:01 +1000

    Initial Commit

---
+++ Study Buddy/Study Buddy/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Study_Buddy.ViewModels;
+
+namespace Study_Buddy.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Study Buddy/Study Buddy/Views/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Study_Buddy.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Study_Buddy.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new LoginViewModel();
+        }
+    }
+}
