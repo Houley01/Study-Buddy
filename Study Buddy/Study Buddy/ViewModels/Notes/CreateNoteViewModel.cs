af34c90b318d0bdae5913e547b3882f0ef0c37d2
Grant Campbell <blindman96@gmail.com>
2020-09-15 22:21:16 +1000

    New Note page and VM

---
+++ Study Buddy/Study Buddy/ViewModels/Notes/CreateNoteViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Study_Buddy.ViewModels;
+using Microsoft.EntityFrameworkCore.Internal;
+using System.Linq;
+
+namespace StudyBuddy.ViewModels
+{
+    public class CreateNoteViewModel : BaseViewModel
+    {
+        private string title;
+        public string NoteTitle      
+        {
+            get => title;
+            set => SetProperty(ref title, value);
+        }
+        private string text;
+        public string NoteContent
+        {
+            get => text;
+            set => SetProperty(ref text, value);
+        }
+        private string[] tags;
+        public string NoteTags
+        {
+            get => string.Join(',', tags);
+            set => SetProperty(ref tags, value.Split(','));
+        }
+
+        private string visibility;
+        public string NoteVisibility
+        {
+            get => visibility;
+            set => SetProperty(ref visibility, value);
+        }
+
+        public ICommand SaveCommand { get; private set; }
+        public ICommand SelectPeopleCMD { get; private set; }
+
+        public CreateNoteViewModel() : base()
+        {
+            title = string.Empty;
+            text = string.Empty;
+            tags = new string[] { };
+            NoteVisibility = "Everyone";
+            //SaveCommand = new Command(async () => await SaveNote());
+        }
+
+        private async Task SaveNote()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Study Buddy/Study Buddy/ViewModels/Notes/NoteViewModel.cs
@@ -0,0 +1,23 @@
+﻿using StudyBuddy.Models.Notes;
+using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Study_Buddy.ViewModels
+{
+    public class NoteViewModel : BaseViewModel
+    {
+        protected Note _Note;
+        public Note NoteObject { get => _Note; set => SetProperty(ref _Note, value); }
+
+        public NoteViewModel()
+        {
+            Title = "View Note";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamain-quickstart"));
+            this._Note = new Note();
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
